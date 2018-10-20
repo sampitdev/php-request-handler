a4b3b5cde5ae44d6e5b5a491727fa47fa3761f51
fery21irawan <developersampit@gmail.com>
2018-10-20 09:55:38 +0700

    first commit

---
+++ app/src/main/java/id/sampitdev/sampitdevlibrary/MainActivity.java
@@ -0,0 +1,15 @@
+package id.sampitdev.sampitdevlibrary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import id.sampitdev.requesthandlerphp.RequestHandler;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
